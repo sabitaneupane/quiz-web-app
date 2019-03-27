845397cfe70dd30d91c51fbc26b6033de045bf21
Sabita neupane <nsabita.sasa@gmail.com>
2019-03-27 20:11:12 +0545

    Adding css file

--- src/PlayQuiz.tsx
+++ src/PlayQuiz.tsx
@@ -3,6 +3,8 @@ import ListQuestions from "./ListQuestions";
 import ListAnswerOptions from "./ListAnswerOptions";
 import AnswerDisplay from "./AnswerDisplay";
 
+import '../styles/main.css';
+
 interface IState{
   quiz_details:Object[];
 }
