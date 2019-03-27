5a29ad738328adac26b3f6a1aee67e66a075041a
Sabita neupane <nsabita.sasa@gmail.com>
2019-03-27 14:27:00 +0545

    basic setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,12 @@ const htmlWebpackPlugin = new HtmlWebPackPlugin({
 });
 
 module.exports = {
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
   module: {
     rules: [
+      {test: /\.tsx?$/, loader: 'ts-loader'},
       {
         test: /\.js$/,
         exclude: /node_modules/,
