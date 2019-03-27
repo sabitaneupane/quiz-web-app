8315103b453394be5083b510bb87d94e2a107a41
Sabita neupane <nsabita.sasa@gmail.com>
2019-03-27 15:35:40 +0545

    creating required files

---
+++ src/AnswerDisplay.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class AnswerDisplay extends React.Component{
+    render() {
+        return (
+            <h2> Answer Display </h2>
+        )
+    }
+}
+
+export default AnswerDisplay;
---
+++ src/ListAnswerOptions.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class ListAnswerOptions extends React.Component{
+    render() {
+        return (
+            <h2> List Answer Option </h2>
+        )
+    }
+}
+
+export default ListAnswerOptions;
