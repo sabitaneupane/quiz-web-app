8dfe63aae24cbef3601d95362fc2c21b647e86c3
Sabita neupane <nsabita.sasa@gmail.com>
2019-05-13 12:30:20 +0545

    Updating some spec

--- react/app/spec/Playquiz.spec.js
+++ react/app/spec/Playquiz.spec.js
@@ -22,35 +22,34 @@ describe('<Playquiz />', () => {
             expect(wrapper.state().scoreAchieved).toBeDefined();
             expect(wrapper.state().isLoading).toBeDefined();
         });
+
+        it('Should have default value for states', () => {
+            expect(wrapper.state().quiz_details).toEqual([]);
+            expect(wrapper.state().totalQuestions).toEqual(0);
+            expect(wrapper.state().questionsCounter).toEqual(0);
+            expect(wrapper.state().question_id).toEqual(0);
+            expect(wrapper.state().question).toEqual('');
+            expect(wrapper.state().answers).toEqual([]);
+            expect(wrapper.state().correctAnswer).toEqual('');
+            expect(wrapper.state().isQuizCompleted).toEqual(false);
+            expect(wrapper.state().selectedAns).toEqual('');
+            expect(wrapper.state().quizSubmitCompleted).toEqual(false);
+            expect(wrapper.state().quizResult).toEqual(false);
+            expect(wrapper.state().scoreAchieved).toEqual(0);
+            expect(wrapper.state().isLoading).toEqual(true);
+        });
     });
 
-    it('Should have default value for states', () => {
-        expect(wrapper.state().quiz_details).toEqual([]);
-        expect(wrapper.state().totalQuestions).toEqual(0);
-        expect(wrapper.state().questionsCounter).toEqual(0);
-        expect(wrapper.state().question_id).toEqual(0);
-        expect(wrapper.state().question).toEqual('');
-        expect(wrapper.state().answers).toEqual([]);
-        expect(wrapper.state().correctAnswer).toEqual('');
-        expect(wrapper.state().isQuizCompleted).toEqual(false);
-        expect(wrapper.state().selectedAns).toEqual('');
-        expect(wrapper.state().quizSubmitCompleted).toEqual(false);
-        expect(wrapper.state().quizResult).toEqual(false);
-        expect(wrapper.state().scoreAchieved).toEqual(0);
-        expect(wrapper.state().isLoading).toEqual(true);
+    describe('Correct elements are present', () => {
+        it('Should have given classes and elements', () => {
+            expect(wrapper.find('.LoadingHead')).toHaveLength(1);
+            // expect(wrapper.find('.container')).toHaveLength(1);
+        });
     });
-
-    //problem
-    // describe('Correct elements are present', () => {
-    //     it('Should have given classes and elements', () => {
-    //         expect(wrapper.find('.container')).toHaveLength(1);
-    //     });
-    // });
     
     describe('Component  ', () => {
         it('should render LoadingHead with appropriate text', () => {
             expect(wrapper.find('.LoadingHead').text()).toBe('Loading...');
-            expect(wrapper.find('.LoadingHead')).toHaveLength(1);
         });
     });
 });
