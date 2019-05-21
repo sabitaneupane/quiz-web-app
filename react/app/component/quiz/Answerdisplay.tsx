bd382cb57b788a408477d5b253815d8801295c1f
Sabita neupane <nsabita.sasa@gmail.com>
2019-05-21 21:15:48 +0545

    removing unwanted spaces

--- react/app/component/quiz/Answerdisplay.tsx
+++ react/app/component/quiz/Answerdisplay.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 const Answerdisplay = props => {
   const { correctAnswer } = props;
-  return <div className="correctAnswer">Correct ans: {correctAnswer}</div>;
+  return <div className="correctAnswer text-danger">Correct ans: {correctAnswer}</div>;
 };
 
 export default Answerdisplay;
